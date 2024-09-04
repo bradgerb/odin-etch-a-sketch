05ec244485a1a7667c671d7bea8b3b03411ea8bc
bradgerb <77023448+bradgerb@users.noreply.github.com>
2024-09-04 14:27:50 -0400

    Create internal boxes function

--- javascript.js
+++ javascript.js
@@ -4,3 +4,12 @@ const sketchpad = document.createElement("div");
 sketchpad.classList.add("sketchpad");
 container.appendChild(sketchpad);
 
+function create_boxes(count){
+    for (let i = 0; i <16; i++){
+        const box = document.createElement("div");
+        box.classList.add("box");
+        sketchpad.appendChild(box);
+    }
+};
+
+create_boxes();
