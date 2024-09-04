654cfe660d1d4b8db8fb2f6cebfed4d651de0d88
bradgerb <77023448+bradgerb@users.noreply.github.com>
2024-09-04 12:42:36 -0400

    Set up initial styling and containers

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".container");
+
+const sketchpad = document.createElement("div");
+sketchpad.classList.add("sketchpad");
+container.appendChild(sketchpad);
+
